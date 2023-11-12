79add8cdb8bfcf28095b3f7a9c58535f775c3021
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-11-12 11:46:34 +0530

    Added sign in & signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,51 @@
 import Header from "./Header";
-
+import { useState } from "react";
 const Login = () => {
-    return (
-        <div>
-            <Header />
-            <div>
-                <img src='https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg' alt='logo'/>
-            </div>
-        </div>
-    )
-}
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="w-4/12 absolute p-12 bg-black my-20 mx-auto right-0 left-0 text-white rounded-md bg-opacity-80">
+        <h1 className="font-semibold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 rounded-md"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign up now"
+            : "Already registered ? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
