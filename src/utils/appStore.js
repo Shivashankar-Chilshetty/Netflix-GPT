1f150fb5bc2646e62c27779a404f8c269549e0df
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-12-15 09:16:24 +0530

    Implemented main container for video playing in background

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from "./userSlice";
+import moviesReducer from './moviesSlices';
 
 const appStore = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        movies: moviesReducer
     }
 });
 
