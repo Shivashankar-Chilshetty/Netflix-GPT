36135fa633fd1cdbc9956959097d26ff88429f9d
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-12-15 11:58:31 +0530

    added popular & top rated movies to the card

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const BG_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg";
 export const USER_AVATAR = "";
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
+
 
 export const API_OPTIONS = {
     method: 'GET',
