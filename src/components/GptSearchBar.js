a7f4666e4a71e8a980bd9ef4364f03fafd394bfa
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-12-23 18:04:11 +0530

    added gpt search functionality & updated readme file

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,9 +1,50 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
+import { useRef } from "react";
+import openai from "../utils/openai";
+import { API_OPTIONS } from "../utils/constants";
+import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
-  const handleGptSearchClick = () => {};
+  const searchText = useRef(null);
+  const dispatch = useDispatch();
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
+
+  const handleGptSearchClick = async () => {
+    const gptQuery = `Act as a Movie Recommendation system and suggest some movies for the query:${searchText?.current?.value}. Only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya`;
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+    // const gptResults = {
+    //   choices: [
+    //     {
+    //       message: {
+    //         role: "assistant",
+    //         content: "Don, Superbad, Bridesmaids, The Hangover, Step Brothers",
+    //       },
+    //     },
+    //   ],
+    // };
+    if (!gptResults.choices) return "Failed to fetch results from gpt apis";
+    const gptMovies = gptResults.choices?.[0]?.message?.content?.split(",");
+    //search each movie in TMDB api's
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    const tmdbResults = await Promise.all(promiseArray);
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
+  };
   return (
     <div className="pt-[10%] flex justify-center">
       <form
@@ -11,6 +52,7 @@ const GptSearchBar = () => {
         onSubmit={(e) => e.preventDefault()}
       >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,21 @@
-export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
-export const BG_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const BG_IMG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg";
 export const USER_AVATAR = "";
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
 export const SUPPORTED_LANGUAGES = [
-  {identifier: "en", name: "English"},
-  {identifier: "hindi", name: "Hindi"},
-  {identifier: "spanish", name: "Spanish"}
-]
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: ''
-    }
-  };
-  
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
+};
